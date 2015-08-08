72995bd99f1544459006cf9b03836b739a0aadd9
linroid <linroid@gmail.com>
2015-08-08 16:44:40 +0800

    [bugfix]设备获得收到的安装包的查询字段不正确

--- app/Http/Controllers/Api/PackageController.php
+++ app/Http/Controllers/Api/PackageController.php
@@ -11,21 +11,18 @@ namespace app\Http\Controllers\Api;
 
 use App\Device;
 use App\Package;
-use App\PushDevice;
-use DB;
 use Response;
 
 class PackageController extends ApiController {
 	public function getIndex() {
 		$device = Device::current();
-//		DB::table('packages')->join()->
 		$packages = Package::join('pushes', 'packages.id', '=', 'pushes.package_id')
 			->join('push_devices', 'pushes.id', '=', 'push_devices.push_id')
 			->where('push_devices.device_id', '=', $device->id)
 			->orderBy('pushes.created_at', 'desc')
 			->groupBy('packages.id')
+			->select('packages.*')
 			->paginate(30);
 		return Response::json($packages);
-//		var_dump($packages);
 	}
 }
