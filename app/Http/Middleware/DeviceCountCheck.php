c9938617f9c420ef41589e2c3cc58846b9de3135
linroid <linroid@gmail.com>
2015-08-16 16:52:50 +0800

    [optimize]需要跳转到绑定设备时提示消息

--- app/Http/Middleware/DeviceCountCheck.php
+++ app/Http/Middleware/DeviceCountCheck.php
@@ -18,7 +18,7 @@ class DeviceCountCheck {
 	public function handle($request, Closure $next) {
 		//如果还没有绑定任何设备，跳转到绑定设备页面
 		if (Device::whereUserId(Auth::id())->count() == 0) {
-			return Redirect::to('/device/bind');
+			return Redirect::to('/device/bind')->withToast('请先绑定至少一台设备');
 		}
 		return $next($request);
 	}
